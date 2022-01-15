1d30395a0740f6a39ed444fad48a6ebf86b837b8
jacobef <jacobef2@gmail.com>
2022-01-15 15:52:58 -0800

    oh my god david

--- grid.py
+++ grid.py
@@ -1,16 +1,11 @@
-from array import *
-
-
 class Grid:
     def __init__(self, rows, cols):
-        self.rows = rows;
-        self.cols = cols;
+        self.rows = rows
+        self.cols = cols
 
         self.vals = []
         for i in range(0, cols):
-            self.vals.append([])
-            for j in range(0, rows):
-                self.vals[i].append(-1)
+            self.vals.append([-1] * rows)
 
     def __str__(self):
         return str(self.vals)
@@ -18,5 +13,5 @@ class Grid:
     def set_val(self, x, y, val):
         self.vals[x][y] = val
 
-    def get_val(self, x, y, val):
+    def get_val(self, x, y):
         return self.vals[x][y]
