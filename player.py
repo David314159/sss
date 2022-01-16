567b99a2a89879a03a5361af805df13d0fd78aa1
jacobef <jacobef2@gmail.com>
2022-01-15 17:16:55 -0800

    add move_region and move_square for Player class

--- player.py
+++ player.py
@@ -1,7 +1,14 @@
 class Player:
-    def __init__(self, x_map: int, y_map: int, x_region: int, y_region: int):
-        self.x_map = x_map
-        self.y_map = y_map
+    def __init__(self, x_region: int, y_region: int, x_square: int, y_square: int):
+        self.x_region = x_region
+        self.y_region = y_region
+        self.x_square = x_square
+        self.y_square = y_square
+
+    def move_region(self, x_region: int, y_region: int):
         self.x_region = x_region
         self.y_region = y_region
 
+    def move_square(self, x_square: int, y_square: int):
+        self.x_square = x_square
+        self.y_square = y_square
