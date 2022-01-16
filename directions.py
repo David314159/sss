c9582373a7dd528ca4acbfb7acd532ee21e2c798
jacobef <jacobef2@gmail.com>
2022-01-16 00:55:38 -0800

    add Direction.str_to_direction

--- directions.py
+++ directions.py
@@ -10,3 +10,19 @@ class Direction(Enum):
     SOUTHEAST = auto()
     NORTHWEST = auto()
     SOUTHWEST = auto()
+
+    @staticmethod
+    # quotes around the "Direction" typehint because python is being cringe
+    # see https://www.python.org/dev/peps/pep-0484/#forward-references
+    def str_to_direction(string: str) -> 'Direction':
+        str_to_direction_dict = {
+            'N': Direction.NORTH,
+            'NE': Direction.NORTHEAST,
+            'E': Direction.EAST,
+            'SE': Direction.SOUTHWEST,
+            'S': Direction.SOUTH,
+            'SW': Direction.SOUTHWEST,
+            'W': Direction.WEST,
+            'NW': Direction.NORTHWEST,
+        }
+        return str_to_direction_dict[string.upper()]
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
 from directions import Direction
 from player import Player
-from region import Region
 
 
 player = Player(0, 0, 0, 0)
 
 while True:
-    print("Which direction do you want to move?")
-    action = input().upper()
-    direction = None
-    match action:
-        case "N":
-            direction = Direction.NORTH
-        case "S":
-            direction = Direction.SOUTH
-        case "E":
-            direction = Direction.EAST
-        case "W":
-            direction = Direction.WEST
-        case "NE":
-            direction = Direction.NORTHEAST
-        case "NW":
-            direction = Direction.NORTHWEST
-        case "SE":
-            direction = Direction.SOUTHEAST
-        case "SW":
-            direction = Direction.SOUTHWEST
-        case _:
-            print("Not a valid direction:")
+    direction = Direction.str_to_direction(
+        input("Which direction do you want to move? ")
+    )
     player.move_squares(direction, 1)
     print(f"X:{player.x_square} Y:{player.y_square}")
 
