eec250d26bbf4544f565225d4295d399d494f7c3
David314159 <dabrams2022@kehillahstudent.org>
2022-01-15 17:11:48 -0800

    fix pychache and add player class

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, x_map: int, y_map: int, x_region: int, y_region: int):
+        self.x_map = x_map
+        self.y_map = y_map
+        self.x_region = x_region
+        self.y_region = y_region
+
