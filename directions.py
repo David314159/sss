aca44145bd5645e38e9b39b87b1920dff2aca700
David314159 <dabrams2022@kehillahstudent.org>
2022-01-17 17:54:11 -0800

    Fix abrv to enum and direction error case

--- directions.py
+++ directions.py
@@ -10,19 +10,28 @@ class Direction(Enum):
     SOUTHEAST = auto()
     NORTHWEST = auto()
     SOUTHWEST = auto()
+    INVALID = auto()
 
     @staticmethod
     # quotes around the "Direction" typehint because python is being cringe
     # see https://www.python.org/dev/peps/pep-0484/#forward-references
-    def abbreviation_to_enum(string: str) -> 'Direction':
-        str_to_direction_dict = {
-            'N': Direction.NORTH,
-            'NE': Direction.NORTHEAST,
-            'E': Direction.EAST,
-            'SE': Direction.SOUTHWEST,
-            'S': Direction.SOUTH,
-            'SW': Direction.SOUTHWEST,
-            'W': Direction.WEST,
-            'NW': Direction.NORTHWEST,
-        }
-        return str_to_direction_dict[string.upper()]
+    def abbreviation_to_enum(abrv: str) -> 'Direction':
+        match abrv.upper():
+            case "N":
+                return Direction.NORTH
+            case "S":
+                return Direction.SOUTH
+            case "E":
+                return Direction.EAST
+            case "W":
+                return Direction.WEST
+            case "NE":
+                return Direction.NORTHEAST
+            case "SE":
+                return Direction.SOUTHEAST
+            case "NW":
+                return Direction.NORTHWEST
+            case "SW":
+                return Direction.SOUTHWEST
+            case _:
+                return Direction.INVALID
--- main.py
+++ main.py
@@ -8,6 +8,13 @@ while True:
     direction = Direction.abbreviation_to_enum(
         input("Which direction do you want to move? ")
     )
+    while direction == Direction.INVALID:
+        print("Invalid direction")
+        direction = Direction.abbreviation_to_enum(
+            input("Which direction do you want to move? ")
+        )
+
     player.move_squares(direction, 1)
+
     print(f"X:{player.x_square} Y:{player.y_square}")
 
